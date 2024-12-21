edce7faaa11fc39904a90b168e18143a9f198007
ragapriya <mohamed.ansari@synergein365.com>
2024-12-21 09:14:07 +0530

    Domain Models created

---
+++ Sample.Datahub/Models/Domain/Branch.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample.Datahub.Models.Domain
+{
+    public class Branch
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string? BranchImageUrl { get; set; }
+    }
+}
---
+++ Sample.Datahub/Models/Domain/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace Sample.Datahub.Models.Domain
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string? EmployeeImageUrl { get; set; }
+        public Guid BranchId { get; set; }
+        public Guid TeamId { get; set; }
+
+        // Navigation Properties
+        public Branch Branch { get; set; }
+        public Team Team { get; set;  }
+
+    }       
+}
