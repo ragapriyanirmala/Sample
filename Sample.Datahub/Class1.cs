afc38cc127d7b75c85ac9afbf6ab04ae4ffa9322
ragapriya <mohamed.ansari@synergein365.com>
2024-12-21 09:15:10 +0530

    DbContext,configuration added

--- Sample.Datahub/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sample.Datahub
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Sample.Datahub/SampleDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Sample.Datahub.Models.Domain;
+
+namespace Sample.Datahub
+{
+    public class SampleDbContext:DbContext
+    {
+        public SampleDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) 
+        {
+                
+        }
+        public DbSet<Branch> Branches { get; set; } 
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
